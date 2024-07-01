9a9ee933b8ebba909d25457c25cbaaf75d346dd7
StevenCA-096 <115894511+StevenCA-096@users.noreply.github.com>
2024-06-30 18:31:54 -0600

    add available/notAvailable appointment endpoints

--- src/appointment/appointment.controller.ts
+++ src/appointment/appointment.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { AppointmentService } from './appointment.service';
 import { CreateAppointmentDto } from './dto/create-appointment.dto';
 import { UpdateAppointmentDto } from './dto/update-appointment.dto';
@@ -25,6 +25,16 @@ export class AppointmentController {
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateAppointmentDto: UpdateAppointmentDto) {
     return this.appointmentService.update(+id, updateAppointmentDto);
+  }
+
+  @Patch('setAvailableAppointment/:id')
+  setAvailableAppointment(@Param('id', ParseIntPipe) id:number){
+    return this.appointmentService.setAvailableAppointment(id);
+  }
+
+  @Patch('setNotAvailableAppointment/:id')
+  setNotAvailableAppointment(@Param('id', ParseIntPipe) id:number){
+    return this.appointmentService.setNotAvailableAppointment(id);
   }
 
   @Delete(':id')
--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -1,7 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateAppointmentDto } from './dto/create-appointment.dto';
 import { UpdateAppointmentDto } from './dto/update-appointment.dto';
-import { Appointment } from './entities/appointment.entity'; 
+import { Appointment } from './entities/appointment.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CounterService } from 'src/counter/counter.service';
@@ -13,37 +13,67 @@ export class AppointmentService {
     @InjectRepository(Appointment)
     private appointmentRepository: Repository<Appointment>,
     private counterService: CounterService
-  ){}
+  ) { }
 
   async create(createAppointmentDto: CreateAppointmentDto) {
     const counterFound = await this.counterService.findOne(createAppointmentDto.counterId)
-    if(!counterFound) throw new HttpException('Counter not found', HttpStatus.NOT_FOUND)
-    return await this.appointmentRepository.save({...createAppointmentDto, counter: counterFound});
+    if (!counterFound) throw new HttpException('Counter not found', HttpStatus.NOT_FOUND)
+    return await this.appointmentRepository.save({ ...createAppointmentDto, counter: counterFound });
   }
 
   async findAll() {
     return await this.appointmentRepository.find({
-      relations: {studentEnrollment: true},
-  });
+      relations: { studentEnrollment: true },
+    });
   }
 
   async findOne(id: number) {
     const appointmentFound = await this.appointmentRepository.findOne({
-    where: {id}, relations: {studentEnrollment: true}});
-    if(!appointmentFound) throw new HttpException(`Appointment with ID ${id} not found`, HttpStatus.NOT_FOUND);
+      where: { id }, relations: { studentEnrollment: true }
+    });
+    if (!appointmentFound) throw new HttpException(`Appointment with ID ${id} not found`, HttpStatus.NOT_FOUND);
     return appointmentFound;
   }
 
   async update(id: number, updateAppointmentDto: UpdateAppointmentDto) {
     const appointmentFound = await this.appointmentRepository.findOneBy({ id });
-    if(!appointmentFound) throw new HttpException(`Appointment with ID ${id} not found`, HttpStatus.NOT_FOUND);
+    if (!appointmentFound) throw new HttpException(`Appointment with ID ${id} not found`, HttpStatus.NOT_FOUND);
     await this.appointmentRepository.update(id, updateAppointmentDto);
     return await this.appointmentRepository.findOneBy({ id });
   }
 
   async remove(id: number) {
     const appointmentFound = await this.appointmentRepository.findOneBy({ id });
-    if(!appointmentFound) throw new HttpException(`Appointment with ID ${id} not found`, HttpStatus.NOT_FOUND);
+    if (!appointmentFound) throw new HttpException(`Appointment with ID ${id} not found`, HttpStatus.NOT_FOUND);
     return await this.appointmentRepository.delete(id);
+  }
+
+  async setNotAvailableAppointment(id: number) {
+    const appointmentFound = await this.appointmentRepository.findOneBy({ id });
+    if (!appointmentFound) throw new HttpException(`Appointment with ID ${id} not found`, HttpStatus.NOT_FOUND);
+
+    if (appointmentFound.taken == false) {
+      try {
+        appointmentFound.taken = true;
+        return this.appointmentRepository.save(appointmentFound)
+      } catch (error) {
+        return error;
+      }
+    } else {
+      throw new HttpException(`Appointment with ID ${id} is taken`, HttpStatus.CONFLICT)
+    }
+  }
+
+  async setAvailableAppointment(id: number) {
+    const appointmentFound = await this.appointmentRepository.findOneBy({ id });
+    if (!appointmentFound) throw new HttpException(`Appointment with ID ${id} not found`, HttpStatus.NOT_FOUND);
+
+    try {
+      appointmentFound.taken = false;
+      return this.appointmentRepository.save(appointmentFound)
+    } catch (error) {
+      return error;
+    }
+
   }
 }
